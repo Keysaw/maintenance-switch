2bdc9e59bef0ae0903cbdb5089d441c0d7cc4d60
lukasbreitinger <lukasbreitinger@proton.me>
2024-11-14 11:21:20 +0100

    Add German Language file

---
+++ resources/lang/de/general.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+	'tooltip' => ':Action Wartungsmodus',
+	'success' => [
+		'title' => 'Wartungsmodus :status',
+		'body' => [
+			'up' => 'Ihre Anwendung ist jetzt live!',
+			'down' => 'Geheimer Token: **:secret**',
+		],
+	],
+	'error' => [
+		'title' => 'Etwas ist schief gelaufen. Der Wartungsmodus konnte nicht :status werden.',
+	],
+	'activate' => 'aktivieren',
+	'deactivate' => 'deaktivieren',
+	'activated' => 'aktiviert',
+	'deactivated' => 'deaktiviert',
+	'up' => 'Oben',
+	'down' => 'Unten',
+];
