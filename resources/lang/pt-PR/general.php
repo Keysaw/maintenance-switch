cee95e8fc4c845047ee3af43cd7baea0bfd91f7c
a21ns1g4ts <atila.danvi@outlook.com>
2024-11-16 15:29:07 -0300

    Add pt translations

---
+++ resources/lang/pt-PR/general.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+	'tooltip' => 'Modo de manutenção :Action',
+	'success' => [
+		'title' => 'Modo de manutenção :status',
+		'body' => [
+			'up' => 'Sua aplicação está agora ativa!',
+			'down' => 'Token Secreto: **:secret**',
+		],
+	],
+	'error' => [
+		'title' => 'Algo deu errado. O modo de manutenção não pôde ser :status.',
+	],
+	'activate' => 'ativar',
+	'deactivate' => 'desativar',
+	'activated' => 'ativado',
+	'deactivated' => 'desativado',
+	'up' => 'Ativo',
+	'down' => 'Inativo',
+];
